e51421b176322d8f649e9c96e4a273330d501265
Chase <chase.jhbaek@kakaocorp.com>
2024-09-27 11:54:18 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `weddingInvitation`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://jhbaek7.github.io/WeddingCard/`
   },
   plugins: ["gatsby-plugin-styled-components"]
 };
