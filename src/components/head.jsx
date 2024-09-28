61aeb1e734ef19f72893906438e030fe26840286
Chase <chase.jhbaek@kakaocorp.com>
2024-09-28 18:00:19 +0900

    head

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -4,7 +4,7 @@ export const Head = () => (
     <>
       <meta property="og:title" content="백주현 & 정희성 청첩장" />
         <meta property="og:description" content="백주현 & 정희성 결혼합니다" />
-        <meta property="og:image" content="https://jhbaek7.github.io/WeddingCard/static/kakaoShare.jpg" />
+        <meta property="og:image" content="https://jhbaek7.github.io/WeddingCard/kakaoShare.jpg" />
         <meta property="og:url" content="https://jhbaek7.github.io/WeddingCard/" /> 
     </>
 )
